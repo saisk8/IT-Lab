8a75f2843957ec5aba12d9b53b31b8d2baf070db
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-03-08 13:01:25 +0530

    Tharvatha antha daya

--- Webpage/js/app.js
+++ Webpage/js/app.js
@@ -1,19 +1,21 @@
-"use strict";
+'use strict';
 
 (function() {
   function init() {
     let router = new Router([
-      new Route("home", "home.html", true),
-      new Route("about", "about.html"),
-      new Route("contact", "contact.html"),
-      new Route("admissions", "admissions.html"),
-      new Route("programmes", "programmes.html"),
-      new Route("faculty", "faculty.html"),
-      new Route("syllabus", "syllabus.html"),
-      new Route("visitors", "visitors.html"),
-      new Route("events", "events.html"),
-      new Route("latest-news", "latest-news.html"),
-      new Route("placements", "placements.html")
+      new Route('home', 'home.html', true),
+      new Route('about', 'about.html'),
+      new Route('contact', 'contact.html'),
+      new Route('admissions', 'admissions.html'),
+      new Route('programmes', 'programmes.html'),
+      new Route('faculty', 'faculty.html'),
+      new Route('syllabus', 'syllabus.html'),
+      new Route('visitors', 'visitors.html'),
+      new Route('events', 'events.html'),
+      new Route('latest-news', 'latest-news.html'),
+      new Route('placements', 'placements.html'),
+      new Route('login', 'login.html'),
+      new Route('register', 'register.html')
     ]);
   }
   init();
--- Webpage/server.js
+++ Webpage/server.js
@@ -9,10 +9,10 @@ const app = express();
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('views'));
-app.use(express.static('js'));
-app.use(express.static('css'));
-app.use(express.static('fonts'));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/js'));
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/fonts'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
