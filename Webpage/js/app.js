2f27953c83327cdc7471b3f66034ff342929d446
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-03-08 09:01:31 +0530

    Update scripts

--- Webpage/js/app.js
+++ Webpage/js/app.js
@@ -1,18 +1,20 @@
-'use strict';
+"use strict";
 
-(function () {
-    function init() {
-        var router = new Router([
-            new Route('home', 'home.html', true),
-            new Route('about', 'about.html'),
-            new Route('contact', 'contact.html'),
-            new Route('admissions', 'admissions.html'),
-            new Route('programmes', 'programmes.html'),
-            new Route('faculty', 'faculty.html')
-
-
-
-        ]);
-    }
-    init();
-}());
+(function() {
+  function init() {
+    let router = new Router([
+      new Route("home", "home.html", true),
+      new Route("about", "about.html"),
+      new Route("contact", "contact.html"),
+      new Route("admissions", "admissions.html"),
+      new Route("programmes", "programmes.html"),
+      new Route("faculty", "faculty.html"),
+      new Route("syllabus", "syllabus.html"),
+      new Route("visitors", "visitors.html"),
+      new Route("events", "events.html"),
+      new Route("latest-news", "latest-news.html"),
+      new Route("placements", "placements.html")
+    ]);
+  }
+  init();
+})();
