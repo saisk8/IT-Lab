b06f7cf0052092383cf28aafec221a782801212c
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-03-08 09:32:04 +0530

    Add new files

---
+++ Webpage/server.js
@@ -0,0 +1,25 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require('express');
+const app = express();
+
+// we've started you off with Express,
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('views'));
+app.use(express.static('js'));
+app.use(express.static('css'));
+app.use(express.static('fonts'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+// listen for requests :)
+const listener = app.listen(3000, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
